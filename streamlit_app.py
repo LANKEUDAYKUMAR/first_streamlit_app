38eeb6a8ec70ca570d7fa5b9fdb0d93816ecc3d7
LANKEUDAYKUMAR <123228581+LANKEUDAYKUMAR@users.noreply.github.com>
2023-01-21 14:18:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.header('Breakfast menu')
 streamlit.text('🥣omega 3, blueberry oatmeal')
 streamlit.text('🥗kale, spinach & rocket smoothe')
 streamlit.text(' 🐔 hard-boiled free range egg')
-streamlit.text('🥑 Avacado toast')                
+streamlit.text('🥑 Avacado toast') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
