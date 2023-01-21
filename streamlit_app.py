8a71df306c642ef20951c5bec4d32fe8256d511c
LANKEUDAYKUMAR <123228581+LANKEUDAYKUMAR@users.noreply.github.com>
2023-01-21 14:08:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3, blueberry oatmeal')
+streamlit.text('kale, spinach & rocket smoothe')
+streamlit.text('hard-boiled free range egg')
