dabbf64b5e520062d9878912e2f966ed06df5b77
LANKEUDAYKUMAR <123228581+LANKEUDAYKUMAR@users.noreply.github.com>
2023-01-23 22:52:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + KIWI")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/ KIWI)
 
 
 # write your own comment -what does the next line do? 
