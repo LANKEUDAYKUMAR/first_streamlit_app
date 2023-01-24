c67b39b3d44895afb4440a50cd03b7a196833ca9
LANKEUDAYKUMAR <123228581+LANKEUDAYKUMAR@users.noreply.github.com>
2023-01-24 16:23:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like to add?','kiwi')
 streamlit.write('Thanks for adding', fruit_choice)
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
       ##fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
